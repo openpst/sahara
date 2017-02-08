7a03c77f6abf49b5aa4c6af182ab50114a9fcfee
Gassan Idriss <ghassani@gmail.com>
2017-02-08 14:53:19 -0800

    license

--- include/task/sahara_image_transfer_task.h
+++ include/task/sahara_image_transfer_task.h
@@ -1,5 +1,21 @@
 /**
-* LICENSE PLACEHOLDER
+*
+* (c) Gassan Idriss <ghassani@gmail.com>
+* 
+* This file is part of sahara.
+* 
+* sahara is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* sahara is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with sahara. If not, see <http://www.gnu.org/licenses/>.
 *
 * @file sahara_image_transfer_task.h
 * @class SaharaImageTransferTask
